9eedf34af116eb7de52e32721f6745609b703486
Emanuel <emanuelbastons@gmail.com>
2019-04-18 23:31:12 -0300

    mensajes en el servidor

--- not_pizzaTCPClient.java
+++ not_pizzaTCPClient.java
@@ -3,10 +3,14 @@ import java.net.*;
 public class not_pizzaTCPClient {
 	public static void main(String argv[]) throws Exception
 		{
+			while (true) {
 			String consulta;
 			String respuesta;
 			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-			Socket clientSocket = new Socket("1111:1111:111:11111:1111:11:11:11", 1234); //Configurar la ip del hostname (se puede obtener aca https://myhostname.net/ )
+			//Configurar la ip del hostname (se puede obtener aca https://myhostname.net/ o bien desde el cmd con ipconfig, usar la ipv6 temporal
+			//por ejemplo: 1111:1111:111:11111:1111:11:11:11
+			Socket clientSocket = new Socket("1111:1111:111:11111:1111:11:11:11", 1234); 
+			
 			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			System.out.println("A continuacion ingrese una palabra para consultar al servidor:");
@@ -15,6 +19,7 @@ public class not_pizzaTCPClient {
 			respuesta = inFromServer.readLine();
 			System.out.println("Respuesta desde el servidor: " + respuesta);
 			clientSocket.close();
+			}
 		}
 	
 }
--- not_pizzaTCPServer.java
+++ not_pizzaTCPServer.java
@@ -1,21 +1,25 @@
 import java.io.*;
 import java.net.*;
+
 class not_pizzaTCPServer {
 	public static void main(String argv[]) throws Exception
 		{
 			String consultaDelCliente;
 			String respuesta;
 			ServerSocket welcomeSocket = new ServerSocket(1234);
-			while(true){
+			
+			System.out.println("Servidor funcionando. Esperando consultas de clientes");
+			while (true) {
 				Socket connectionSocket = welcomeSocket.accept();
 				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 				DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 				consultaDelCliente = inFromClient.readLine();
 				if (consultaDelCliente.equals("pizza")) {
-					respuesta = "¡Si es pizza!" + '\n';
+					respuesta = "Si es pizza!" + '\n';
 				} else {
 					respuesta = "No es pizza.. :(" + '\n';
 				}
+				System.out.println("Se consulto por: " + consultaDelCliente + ". La respuesta fue enviada.");
 				outToClient.writeBytes(respuesta);
 			}
 		}
